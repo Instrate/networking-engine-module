86796ae9769256f2a9b9dc2eb6446f789da3be62
Bohdan Savchuk Ihorovych <antipich69@gmail.com>
2025-05-10 06:40:21 +0300

    fix: incorrect folder usage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import ignore from "./.style/ignore.default.mjs";
 import { JsConfig, JsonConfig, TsConfig } from "style-eslint/lint.mjs";
 import path from "node:path";
 
-const stylesFolder = "./styles";
+const stylesFolder = "./.style";
 
 const ignoreMainList = ignore.retrieveIgnoreList();
 
